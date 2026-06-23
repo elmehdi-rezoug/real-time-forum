d1feb825843ccd5424e685842f2630dbef9f4a3a
halhyane <houssamalhyan5@gmail.com>
2026-06-23 13:48:52 +0100

    add categorie and post table

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,3 +1,4 @@
+
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nickname TEXT UNIQUE NOT NULL,
@@ -8,4 +9,36 @@ CREATE TABLE IF NOT EXISTS users (
     email TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE IF NOT EXISTS sessions (
+    id TEXT PRIMARY KEY UNIQUE,
+    expires_at DATETIME NOT NULL,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE IF NOT EXISTS categories (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE NOT NULL
+);
+
+-- Seed lookup values securely
+INSERT OR IGNORE INTO categories (id, name) VALUES
+(1, 'General'),
+(2, 'Programming'),
+(3, 'Gaming'),
+(4, 'Movies'),
+(5, 'Sports');
+
+CREATE TABLE IF NOT EXISTS posts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    content TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
+    category_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE RESTRICT
 );
