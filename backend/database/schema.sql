ebc238a696f8c27747509e5459098d01cdeffdd5
erezzoug <rezougelmehdi0@gmail.com>
2026-07-20 12:14:14 +0100

    fixing milliseconds in chat

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS messages (
     sender_id INTEGER NOT NULL,
     receiver_id INTEGER NOT NULL,
     content TEXT NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    created_at DATETIME NOT NULL DEFAULT (strftime('%Y-%m-%d %H:%M:%f', 'now')),
     FOREIGN KEY (sender_id) REFERENCES users(id),
     FOREIGN KEY (receiver_id) REFERENCES users(id)
 );
